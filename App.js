2558e17a7ef7f0ebfa1795688eb7ee1383c80dfb
SDK <50303777+sdk-bens@users.noreply.github.com>
2024-07-27 02:58:52 -0400

    Setting up mongodb

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
@@ -6,9 +9,13 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignment/routes.js";
 
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
